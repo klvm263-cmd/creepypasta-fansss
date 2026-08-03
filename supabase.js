94c5975bd4f12e81306c394ee446aab153f5e5df
klvm263-cmd <klvm263@gmail.com>
2026-08-02 20:40:14 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,11 @@
 // Creepypasta Fansss - Supabase Connection
 
 // Put your Supabase Project URL between the quotes
-const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE";
+const SUPABASE_URL = https://qeehdjfblfemzpeqsxwj.supabase.co/rest/v1/;
 
 
 // Put your Supabase anon public key between the quotes
-const SUPABASE_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZWhkamZibGZlbXpwZXFzeHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3MDYyODUsImV4cCI6MjEwMTI4MjI4NX0.oENNPZmlQiye3NQx7YuzLc1SGmT_u1vWMovmzQQP9iQ;
 
 
 // Creates the connection to Supabase
